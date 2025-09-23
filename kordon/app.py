037a5a03fef8e0abd25a730296d5425c877c1421
rubenanko <rubenanko@gmail.com>
2025-09-23 11:04:07 +0200

    feat(): llm request

--- kordon/app.py
+++ kordon/app.py
@@ -1,13 +1,18 @@
 from flask import Flask,request
 from flask_cors import CORS
+from database import *
+import ollama
 
 import configparser
 import time
 import os
 
+
 #flask
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+CONFIG["AVAILABLE_LLMS"] = CONFIG["AVAILABLE_LLMS"].split(" ")
 
 #config
 CONFIG_PATH = os.path.join(os.path.split(os.path.realpath(__file__))[0],"config.ini")
@@ -18,4 +23,18 @@ CONFIG.read(CONFIG_PATH)
 def login():
     token = request.get_json()["token"]
     response = {"success": True } if token == "test" else {"success": False }
+    return response
+
+@app.route('/api/v1/request/llm',methods=['POST'])
+def login():
+    token = request.get_json()["token"]
+    model = request.get_json()["model"]
+    messages = request.get_json()["messages"]
+
+    if check_token(token):
+        if model in CONFIG["AVAILABLE_LLMS"]:
+            response = ollama.chat(model=model, messages=messages, stream=True)
+        response = {"response" : response['message']['content'], "status": "success" }
+        else:   response = { "status": "unknown_model" }
+    else:   response = { "status": "unknown_token" }
     return response
---
+++ kordon/database.py
@@ -0,0 +1,3 @@
+
+def check_token(token : str):
+    return token == "test"
