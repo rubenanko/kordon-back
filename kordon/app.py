406737ca6d0d37b616a29105a7bc658fcff2cb29
Ryan Bouchou <ryanbouchou@orange.fr>
2025-09-24 10:04:44 +0200

    Fix: identation, missing ollama in requirement, and chat method

--- kordon/app.py
+++ kordon/app.py
@@ -1,32 +1,32 @@
-from flask import Flask,request
-from flask_cors import CORS
-from database import *
-import ollama
-
 import configparser
-import time
 import os
+import time
 
+import ollama
+from database import *
+from flask import Flask, request
+from flask_cors import CORS
 
-#flask
+# flask
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-CONFIG["AVAILABLE_LLMS"] = CONFIG["AVAILABLE_LLMS"].split(" ")
-
-#config
-CONFIG_PATH = os.path.join(os.path.split(os.path.realpath(__file__))[0],"config.ini")
+# config
+CONFIG_PATH = os.path.join(os.path.split(os.path.realpath(__file__))[0], "config.ini")
 CONFIG = configparser.ConfigParser()
 CONFIG.read(CONFIG_PATH)
+CONFIG["AVAILABLE_LLMS"] = CONFIG["AVAILABLE_LLMS"].split(" ")
 
-@app.route('/api/v1/auth/login',methods=['POST'])
+
+@app.route("/api/v1/auth/login", methods=["POST"])
 def login():
     token = request.get_json()["token"]
-    response = {"success": True } if token == "test" else {"success": False }
+    response = {"success": True} if token == "test" else {"success": False}
     return response
 
-@app.route('/api/v1/request/llm',methods=['POST'])
-def login():
+
+@app.route("/api/v1/request/llm", methods=["POST"])
+def chat():
     token = request.get_json()["token"]
     model = request.get_json()["model"]
     messages = request.get_json()["messages"]
@@ -34,7 +34,9 @@ def login():
     if check_token(token):
         if model in CONFIG["AVAILABLE_LLMS"]:
             response = ollama.chat(model=model, messages=messages, stream=True)
-        response = {"response" : response['message']['content'], "status": "success" }
-        else:   response = { "status": "unknown_model" }
-    else:   response = { "status": "unknown_token" }
+            response = {"response": response["message"]["content"], "status": "success"}
+        else:
+            response = {"status": "unknown_model"}
+    else:
+        response = {"status": "unknown_token"}
     return response
